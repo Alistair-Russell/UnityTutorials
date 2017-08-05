a948a5427545de46f780d4d8f468373cf15f77a0
Alistair Russell <russell.alistair@gmail.com>
2017-08-05 12:53:55 -0700

    Tutorial1: Roll a ball.

---
+++ Roll Ball/Assets/scripts/cameracontroller.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour {
+
+	// Reference to the player game object
+	public GameObject player;
+
+	// Offset for camera/player
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position;
+	}
+	
+	// LateUpdate is called once per frame AFTER UPDATE
+	// We use LateUpdate because we know that the player has definitely
+	// moved by the time this gets called.
+	void LateUpdate () {
+		transform.position = player.transform.position + offset;
+	}
+}
---
+++ Roll Ball/Assets/scripts/playercontroller.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class playercontroller : MonoBehaviour {
+	// Controls for moving the player object
+
+	// Reference to the player rigidbody
+	private Rigidbody rb;
+	private int count;
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	void Start() {
+		// Called at the start of the game to initialize rb to 
+		// the object we are using.
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate () {
+		// Updates physics ingame when we move the player object
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+		
+	void OnTriggerEnter (Collider other) {
+		// Sets pickups to inactive on collision
+		if (other.gameObject.CompareTag ("Pickup")) {
+			other.gameObject.SetActive (false);
+			count++;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText () {
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 15) {
+			winText.text = "You win!";
+		}
+	}
+
+}
